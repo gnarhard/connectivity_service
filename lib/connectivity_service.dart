99492a2743ce32e2406660af8e10ea228b4c182b
Grayson Erhard <develop@graysonerhard.com>
2023-01-23 11:50:24 -0700

    first draft

---
+++ lib/connectivity_service.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library connectivity_service;
+
+export 'src/connectivity_service_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
---
+++ lib/src/connectivity_service_base.dart
@@ -0,0 +1,18 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:rxdart/rxdart.dart' show BehaviorSubject;
+
+class ConnectivityService {
+  final state$ =
+      BehaviorSubject<ConnectivityResult>.seeded(ConnectivityResult.none);
+  final _connectivity = Connectivity();
+  bool listenersEnabled = false;
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  void init() {
+    _connectivity.onConnectivityChanged
+        .distinct()
+        .listen((ConnectivityResult connectionStatus) {
+      state$.add(connectionStatus);
+    });
+  }
+}
