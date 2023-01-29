3b82ec91523da2cb55b3fd95264d1d42589c1907
Grayson Erhard <develop@graysonerhard.com>
2023-01-29 12:16:37 -0700

    wrote a test for the hasConnectivity getter

--- test/connectivity_service_test.dart
+++ test/connectivity_service_test.dart
@@ -1,5 +1,17 @@
+import 'package:connectivity_service/connectivity_service.dart';
+import 'package:flutter/material.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('A group of tests', () {});
+  setUp(() => WidgetsFlutterBinding.ensureInitialized());
+  test('connection state returns false', () {
+    // arrange
+    final service = ConnectivityService();
+
+    // act
+    service.init();
+
+    // assert
+    expect(service.hasConnectivity, false);
+  });
 }
