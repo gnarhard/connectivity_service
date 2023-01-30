c99546290b4e1a564fbbf371037b848418ad9420
Grayson Erhard <develop@graysonerhard.com>
2023-01-29 19:01:10 -0700

    improved testing coverage

--- test/connectivity_service_test.dart
+++ test/connectivity_service_test.dart
@@ -1,3 +1,4 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:connectivity_service/connectivity_service.dart';
 import 'package:flutter/material.dart';
 import 'package:test/test.dart';
@@ -13,5 +14,18 @@ void main() {
 
     // assert
     expect(service.hasConnectivity, false);
+  });
+
+  test('connection state returns true', () {
+    // arrange
+    final service = ConnectivityService();
+
+    // act
+    service.init();
+    service.state$.add(ConnectivityResult.wifi);
+
+    // assert
+    expectLater(service.state$.stream, emits(ConnectivityResult.wifi));
+    expect(service.hasConnectivity, true);
   });
 }
