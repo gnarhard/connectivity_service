56f5802e95077c9b9381806839e8779487365482
Grayson Erhard <develop@graysonerhard.com>
2023-02-01 11:20:06 -0700

    updated tests

--- test/connectivity_service_test.dart
+++ test/connectivity_service_test.dart
@@ -6,25 +6,13 @@ import 'package:test/test.dart';
 void main() {
   setUp(() => WidgetsFlutterBinding.ensureInitialized());
   test('connection state returns false', () {
-    // arrange
     final service = ConnectivityService();
-
-    // act
-    service.init();
-
-    // assert
     expect(service.hasConnectivity, false);
   });
 
   test('connection state returns true', () {
-    // arrange
     final service = ConnectivityService();
-
-    // act
-    service.init();
     service.state$.add(ConnectivityResult.wifi);
-
-    // assert
     expectLater(service.state$.stream, emits(ConnectivityResult.wifi));
     expect(service.hasConnectivity, true);
   });
