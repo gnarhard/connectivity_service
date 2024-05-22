ee53a3c530deff1a6c36e6e62840c87fa481a979
Grayson Erhard <develop@graysonerhard.com>
2024-05-22 09:33:18 -0600

    updated plugins and allowed override of connectivity status

--- lib/src/connectivity_service_base.dart
+++ lib/src/connectivity_service_base.dart
@@ -3,13 +3,16 @@ import 'package:flutter/foundation.dart';
 import 'package:rxdart/rxdart.dart' show BehaviorSubject;
 
 class ConnectivityService {
+  ConnectivityService({this.overrideConnectivity = false});
+
+  final bool overrideConnectivity;
   final _connectivity = Connectivity();
   bool listenersEnabled = false;
 
   final state$ = BehaviorSubject<List<ConnectivityResult>>.seeded(
       [ConnectivityResult.none]);
 
-  bool get hasConnectivity => !state$.value.contains(ConnectivityResult.none);
+  bool get hasConnectivity => overrideConnectivity ? true : !state$.value.contains(ConnectivityResult.none);
 
   void init() {
     _connectivity.onConnectivityChanged
