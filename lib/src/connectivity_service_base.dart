8583bb1bf39ea83b384c23466565e5d48667e00f
Grayson Erhard <develop@graysonerhard.com>
2024-07-02 23:47:45 -0500

    allow overriding connectivity value

--- lib/src/connectivity_service_base.dart
+++ lib/src/connectivity_service_base.dart
@@ -3,16 +3,17 @@ import 'package:flutter/foundation.dart';
 import 'package:rxdart/rxdart.dart' show BehaviorSubject;
 
 class ConnectivityService {
-  ConnectivityService({this.overrideConnectivity = false});
+  ConnectivityService({this.overrideConnectivity = false, this.overrideConnectivityValue = false});
 
   final bool overrideConnectivity;
+  final bool overrideConnectivityValue;
   final _connectivity = Connectivity();
   bool listenersEnabled = false;
 
   final state$ = BehaviorSubject<List<ConnectivityResult>>.seeded(
       [ConnectivityResult.none]);
 
-  bool get hasConnectivity => overrideConnectivity ? true : !state$.value.contains(ConnectivityResult.none);
+  bool get hasConnectivity => overrideConnectivity ? overrideConnectivityValue : !state$.value.contains(ConnectivityResult.none);
 
   void init() {
     _connectivity.onConnectivityChanged
