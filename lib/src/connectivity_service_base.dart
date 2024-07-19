024b6e28a2dc0739821c15a585e72b29482c5f3c
Grayson Erhard <develop@graysonerhard.com>
2024-07-19 10:12:27 -0400

    add 'allow network toggling' option

--- lib/src/connectivity_service_base.dart
+++ lib/src/connectivity_service_base.dart
@@ -3,23 +3,33 @@ import 'package:flutter/foundation.dart';
 import 'package:rxdart/rxdart.dart' show BehaviorSubject;
 
 class ConnectivityService {
-  ConnectivityService({this.overrideConnectivity = false, this.overrideConnectivityValue = false});
+  ConnectivityService({
+    this.overrideConnectivity = false,
+    this.overrideConnectivityValue = false,
+    this.allowNetworkToggling = true,
+  });
 
   final bool overrideConnectivity;
   final bool overrideConnectivityValue;
+  final bool allowNetworkToggling;
   final _connectivity = Connectivity();
   bool listenersEnabled = false;
 
   final state$ = BehaviorSubject<List<ConnectivityResult>>.seeded(
       [ConnectivityResult.none]);
 
-  bool get hasConnectivity => overrideConnectivity ? overrideConnectivityValue : !state$.value.contains(ConnectivityResult.none);
+  bool get hasConnectivity => overrideConnectivity
+      ? overrideConnectivityValue
+      : !state$.value.contains(ConnectivityResult.none);
 
   void init() {
     _connectivity.onConnectivityChanged
         .distinct()
         .listen((List<ConnectivityResult> connectionStatus) {
+      if (!allowNetworkToggling) return;
+
       debugPrint('Connectivity: $connectionStatus');
+
       state$.add(connectionStatus);
     });
   }
